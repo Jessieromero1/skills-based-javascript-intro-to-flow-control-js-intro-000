d16e3255c7519d2bbf497faf0f6f408cc3a640d8
jessieromero <jessier37706@students.laalliance.org>
2016-11-02 16:01:04 +0000

    Second code saved from nitrous

---
+++ index.js
@@ -0,0 +1,25 @@
+// write your code below!
+
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19 ){
+     return 'You are a teenager!'
+  } else {
+     'You are not a teenager!'
+  }
+}
+
+function teenager(age) {
+
+}
+
+function ageChecker(age) {
+
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
